e0f581d01811b06df31828518ef9c6d6f6057475
Tsuki <i@sukitsuki.com>
2017-04-02 10:06:38 +0800

    enhance web hook

--- bot/tgbot/src/main/kotlin/com/sukitsuki/tgbot/Main.kt
+++ bot/tgbot/src/main/kotlin/com/sukitsuki/tgbot/Main.kt
@@ -1,16 +1,25 @@
 package com.sukitsuki.tgbot
 
+import mu.KotlinLogging
+import org.yanex.telegram.TelegramHoopingBot
 import org.yanex.telegram.TelegramPollingBot
 import org.yanex.telegram.TelegramProperties
 import org.yanex.telegram.entities.Message
 import org.yanex.telegram.entities.Update
+import org.yanex.telegram.extention.toStrings
 import org.yanex.telegram.handler.AbstractUpdateVisitor
 import org.yanex.telegram.handler.StopProcessingException
 import org.yanex.telegram.handler.VisitorUpdateHandler
 
+private val logger = KotlinLogging.logger {}
 fun main(args: Array<String>) {
+
     val properties = TelegramProperties()
-    val bot = TelegramPollingBot.create(token = properties.token)
+    logger.info { properties }
+    val bot = when {
+        properties.webHook -> TelegramHoopingBot.create(properties = properties)
+        else -> TelegramPollingBot.create(properties = properties)
+    }
     val lastId = bot.listen(properties.lastId, VisitorUpdateHandler(object : AbstractUpdateVisitor() {
         override fun visitText(update: Update, message: Message, text: String) = when (text) {
             "ping" -> {
